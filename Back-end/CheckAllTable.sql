695443c677790ce2ed5874f61381f74e57662458
Thiendeptrai169 <127064124+Thiendeptrai169@users.noreply.github.com>
2025-04-06 13:20:31 +0700

    add file sql

---
+++ Back-end/CheckAllTable.sql
@@ -0,0 +1,57 @@
+SELECT *
+FROM [TopicAssignment].[dbo].[Accounts]
+
+SELECT *
+FROM [TopicAssignment].[dbo].[Class]
+
+SELECT *
+FROM [TopicAssignment].[dbo].[Enrollment]
+
+SELECT *
+FROM [TopicAssignment].[dbo].[Faculty]
+
+SELECT *
+FROM [TopicAssignment].[dbo].[FacultyMembers]
+
+SELECT *
+FROM [TopicAssignment].[dbo].[GroupMembers]
+
+SELECT *
+FROM [TopicAssignment].[dbo].[Lecturers]
+
+SELECT *
+FROM [TopicAssignment].[dbo].[LecturerSubjects]
+
+SELECT *
+FROM [TopicAssignment].[dbo].[Major]
+
+SELECT *
+FROM [TopicAssignment].[dbo].[Notification]
+
+SELECT *
+FROM [TopicAssignment].[dbo].[Projects]
+
+SELECT *
+FROM [TopicAssignment].[dbo].[Rights]
+
+SELECT *
+FROM [TopicAssignment].[dbo].[Roles]
+
+SELECT *
+FROM [TopicAssignment].[dbo].[RolesRights]
+
+SELECT *
+FROM [TopicAssignment].[dbo].[Semesters]
+
+SELECT *
+FROM [TopicAssignment].[dbo].[StudentGroups]
+
+SELECT *
+FROM [TopicAssignment].[dbo].[Students]
+
+SELECT *
+FROM [TopicAssignment].[dbo].[Subjects]
+
+
+
+
